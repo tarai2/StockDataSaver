97ad158dd7609b31a743e6a136a2612c8a1b0fe9
tarai2 <takumi.arai2@gmail.com>
2020-07-13 20:30:20 +0900

    remove options

--- setup.py
+++ setup.py
@@ -16,13 +16,12 @@ setup(
     version="0.0.0",
     description="パッケージの説明",
     author="tarai",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-    include_package_data=True,
-    zip_safe=False,
+    # packages=find_packages("src"),
+    # package_dir={"": "src"},
+    # py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    # include_package_data=True,
+    # zip_safe=False,
     install_requires=_requires_from_file('requirements.txt'),
-    setup_requires=["pytest-runner"],
     tests_require=["pytest"]
 )
 
