4311e8a9ab5a2185730c39e79cf89c8ba1378cea
tarai2 <takumi.arai2@gmail.com>
2020-07-13 20:20:38 +0900

    publish setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,32 @@
-from setuptools import setup,find_packages
+import json
+from glob import glob
+from os.path import basename,dirname
+from os.path import splitext
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
 
 
 setup(
     name="stockdatasaver",
     version="0.0.0",
-    install_requires=[
-        "numpy",
-        "pandas",
-        "pytest",
-        "pytest-mock",
-    ],
-    packages=find_packages(where='./'),
-    package_dir={'': './'},
+    description="パッケージの説明",
+    author="tarai",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=_requires_from_file('requirements.txt'),
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"]
 )
 
-
-import json
-with open('config.json', 'w') as file:
+with open(dirname(__file__)+'/config.json', 'w') as file:
     default_config = {
         "homedir": "./",
         "investing.com": [],
