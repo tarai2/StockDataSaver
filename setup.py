11ba0ab9b4f08b55972ea7fde7ebb956adabf4c9
tarai2 <takumi.arai2@gmail.com>
2020-07-13 20:08:45 +0900

    setuptest

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 
 setup(
@@ -9,7 +9,9 @@ setup(
         "pandas",
         "pytest",
         "pytest-mock",
-    ]
+    ],
+    packages=find_packages(where='./'),
+    package_dir={'': './'},
 )
 
 
