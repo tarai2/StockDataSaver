5d382a922de75fa5da003ca2d82387b3ed2df33a
tarai2 <takumi.arai2@gmail.com>
2020-07-13 22:29:41 +0900

    rename package to stockstocker

--- setup.py
+++ setup.py
@@ -6,14 +6,17 @@ from os.path import splitext
 from setuptools import setup
 from setuptools import find_packages
 
+import src
 
 def _requires_from_file(filename):
     return open(filename).read().splitlines()
 
 
 setup(
-    name="stockdatasaver",
-    version="0.0.1",
+    name="stockstocker",
+    version=src.__version__,
+    packages=['stockstocker'],  # import可能な名前空間を指定
+    package_dir={'stockstocker': 'src'},  # 名前空間とディレクトリsrcの対応
     install_requires=_requires_from_file('requirements.txt')
 )
 
